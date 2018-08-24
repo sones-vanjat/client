7576ea592a48ae8ea14c051aeb52708fe73c30d2
wisnugautama <ngurahwisnugautama@gmail.com>
2018-08-24 09:19:33 +0700

    create room & player

---
+++ src/components/homepage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <h1>sones vanjat</h1>
+      <div class="row">
+        <div class="input-field col s6">
+            <input  v-model="playerName" id="first_name2" type="text" class="validate">
+            <label class="active">Player Name</label>
+        </div>
+      </div>
+
+      <div class="row">
+        <div class="input-field col s6">
+            <input v-model="room" id="first_name2" type="text" class="validate">
+            <label class="active">Room Name</label>
+        </div>
+       </div>
+       <a class="waves-effect waves-light btn" @click="addRoom">submit</a>
+    </div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+    data() {
+        return {
+            room: '',
+            playerName: ''
+        }
+    },
+    methods: {
+        ...mapActions([
+            'createRoom'
+        ]),
+        addRoom: function () {
+            // this.$store.dispatch('createRoom', {
+            //     player: this.playerName,
+            //     room: this.room
+            // })
+            let new_room = {
+                room: this.room,
+                player: this.playerName
+            }
+            this.createRoom(new_room)
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+    
+</style>
--- src/store.js
+++ src/store.js
@@ -1,16 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from './firebase'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    roomName: '',
+    player: ''
   },
   mutations: {
-
+    addPlayerName (state, payload) {
+      state.playerName = payload
+    }
   },
   actions: {
+    createRoom (context, data) {
+      let player = data.player
+        let room = data.room
+        db.ref('rooms/' + room).once('value').then(function(snapshot){
+            // console.log(snapshot.val());
+            let cek = snapshot.val()
+            if (cek == null) {
+                db.ref('rooms/' + room + '/player1').set({
+                    player: player,
+                    position: '-190px 0 0 -40px',
+                })
+                this.$route.push('/room')
+            }else if (!cek.player2 && cek.player1 !== '') {
+                db.ref('rooms/' + room + '/player2').set({
+                    player: player,
+                    position: '-190px 0 0 -40px'
+                })
+                this.$route.push('/room')
+            }
+        });
+    },
+    updatePosition () {
 
+    }
   }
 })
