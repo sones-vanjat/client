c62cac2a87168caf867379833e16e7d52544a822
HelmiYogantara <helmiyogantara@gmail.com>
2018-08-24 15:28:23 +0700

    add waiting room

---
+++ src/views/Room.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="waitingRoom">
+        <waiting/>
+    </div>
+</template>
+
+<script>
+import waiting from '@/components/Waiting.vue'
+export default {
+    name: 'waitingRoom',
+    components: {
+        waiting
+    }
+}
+</script>
+
+<style>
+
+</style>
